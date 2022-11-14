39c5aa1298afbe532d9a5ff486083de6c24b477c
Michel Hamelink <info@michelhamelink.nl>
2022-11-14 14:53:42 +0100

    fixed hero responsive

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -2,6 +2,7 @@
 </script>
 
 <template>
+    <div class="mobile-header-vector"></div>
     <div class="container-fluid d-flex flex-column align-items-center">
         <h1>Wij ontwikkelen herbruikbare verpakkingen en circulaire
             <wbr>
@@ -9,58 +10,90 @@
         </h1>
         <button class="secondary">Meer</button>
         <div class="card">
-<!--            <img src="@/assets/images/home-hero-main-image.png" alt="">-->
+            <!--            <img src="@/assets/images/home-hero-main-image.png" alt="">-->
         </div>
     </div>
 </template>
 
 <style scoped>
 .container-fluid {
-    padding: 0 180px;
+    height: calc(100vh - 115px);
+    position: relative;
 }
+
+.mobile-header-vector {
+    overflow: hidden;
+    position: absolute;
+    z-index: -100;
+    width: 150%;
+    left: -25%;
+    height: 60vh;
+    border-radius: 0 0 490% 490%;
+    background-color: var(--primary);
+}
+
+h1 {
+    text-align: center;
+    margin-top: 15px;
+    max-width: 1280px;
+}
+
+button {
+    margin-top: 8px;
+    margin-bottom: 15px;
+}
+
+.card {
+    display: flex;
+    width: 100%;
+    aspect-ratio: 523 / 390;
+    border: none;
+    border-radius: 30px;
+    border-bottom: 7px solid var(--primary);
+    background-image: url("@/assets/images/home-hero-main-image.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 80% 80%;
+    object-fit: contain;
+    margin: 2% 10px 25px 10px;
+}
+
+@media (min-width: 576px) {
+    .card {
+        height: 100%;
+        width: unset;
+    }
+}
+
 @media (min-width: 992px) {
     .container-fluid {
-        height: calc(100vh - 115px);
+        padding: 0 180px;
         background-image: url("@/assets/images/header-desktop-vector.svg");
         background-repeat: no-repeat;
         background-size: 100%;
         background-position: top left;
+    }
+    
+    .mobile-header-vector {
+        display: none;
+    }
+    
+    h1 {
+        margin-top: 40px;
+    }
+    
+    button {
+        margin-top: 30px;
+    }
+    
+    .card {
+        height: 100%;
+        width: unset;
     }
 }
 @media (min-width: 1400px) {
     .container-fluid {
         background-position: top -20vh left;
     }
-}
-h1 {
-    max-width: 1280px;
-    text-align: center;
-    margin-top: 60px;
-}
-button {
-    margin-top: 35px;
-}
-
-.card {
-    margin: 2% 0 25px 0;
-    display: flex;
-    height: 100%;
-    aspect-ratio: 523 / 390;
-    border: none;
-    border-radius: 30px;
-    border-bottom: 7px solid var(--primary);
-
-    background-image: url("@/assets/images/home-hero-main-image.png");
-    background-repeat: no-repeat;
-    background-size: 80% 80%;
-    background-position: center;
-}
-.card img {
-    position: relative;
-    width: auto;
-    height: auto;
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: cover;
 }
 </style>
