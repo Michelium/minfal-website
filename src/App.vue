4fa7af7fe09f4c3baa9e8cbfae629a5a1a27fcb9
Michel Hamelink <info@michelhamelink.nl>
2022-11-26 17:29:51 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {RouterView} from "vue-router";
 import Header from "./components/Header.vue";
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
@@ -8,6 +9,9 @@ import Header from "./components/Header.vue";
         <Header/>
     </section>
     <RouterView/>
+    <section id="footer">
+        <Footer/>
+    </section>
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="container-fluid">
+        <p class="d-none d-lg-block">Minfal © 2022. All rights reserved. | Turfschipper 62, 2292 JB Wateringen</p>
+        <p class="d-lg-none">Minfal © 2022. All rights reserved. <br>Turfschipper 62, 2292 JB Wateringen</p>
+    </footer>
+</template>
+
+<style scoped>
+.container-fluid {
+    padding: 12px 10px;
+    background-color: var(--secondary);
+}
+p {
+    margin-bottom: 0;
+    color: var(--white);
+    font-style: normal;
+    font-weight: 800;
+    font-size: 14px;
+    line-height: 26px;
+    text-align: center;
+}
+@media (min-width: 992px) {
+    p {
+        font-size: 16px;
+    }
+}
+</style>
