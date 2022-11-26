a58760d118379558cffaff0ffb5d4f5ddfbfa5c2
Michel Hamelink <info@michelhamelink.nl>
2022-11-26 17:57:30 +0100

    updated contact form now working

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,41 +1,34 @@
-<script setup>
-defineProps({
-    textareaLabel: {
-        type: String,
-        default: 'Bericht',
-    },
-    fullColls: {
-        type: Boolean,
-        default: false,
-    }
-})
-</script>
-
 <template>
-    <form action="#">
+    <form action="" @submit.prevent="sendEmail">
+        <div v-if="success" class="alert alert-success alert-dismissible fade show" role="alert">
+            <p class="text-dark">Uw bericht is verstuurd! We komen zo spoedig mogelijk bij u terug!</p>
+        </div>
+        <div v-if="error" class="alert alert-danger alert-dismissible fade show" role="alert">
+            <p class="text-dark">Er is iets misgegaan met het versturen. Probeer het later opnieuw of stuur een e-mail!</p>
+        </div>
         <div class="row">
             <div class="col-12 col-lg-6" :class="[fullColls ? 'col-lg-12' : '']">
                 <label for="name">Uw naam</label>
-                <input class="form-control" type="text" v-model="name" id="name">
+                <input class="form-control" type="text" v-model="name" id="name" name="name" required>
             </div>
             <div class="col-12 col-lg-6" :class="[fullColls ? 'col-lg-12' : '']">
                 <label for="company">Bedrijfsnaam</label>
-                <input class="form-control" type="text" v-model="company" id="company">
+                <input class="form-control" type="text" v-model="company" name="company" id="company">
             </div>
             <div class="col-12 col-lg-6" :class="[fullColls ? 'col-lg-12' : '']">
                 <label for="email">E-mail</label>
-                <input class="form-control" type="email" v-model="email" id="email">
+                <input class="form-control" type="email" v-model="email" name="email" id="email" required>
             </div>
             <div class="col-12 col-lg-6" :class="[fullColls ? 'col-lg-12' : '']">
                 <label for="phone">Telefoonnummer</label>
-                <input class="form-control" type="tel" v-model="phone" id="phone">
+                <input class="form-control" type="tel" v-model="phone" name="phone" id="phone">
             </div>
             <div class="col-12">
                 <label for="message">{{ textareaLabel }}</label>
-                <textarea class="form-control" rows="3" v-model="message" id="message" required></textarea>
+                <textarea class="form-control" rows="3" v-model="message" name="message" id="message" required></textarea>
             </div>
             <div class="col-12">
-                <button class="secondary" type="submit">Verstuur</button>
+                <button :disabled="!loaded" class="secondary" type="submit">Verstuur</button>
             </div>
         </div>
     </form>
@@ -62,5 +55,54 @@ form textarea {
 form button {
     margin-top: 40px;
 }
-
 </style>
+
+<script setup>
+import emailjs from 'emailjs-com';
+import {ref} from "vue";
+
+defineProps({
+    textareaLabel: {
+        type: String,
+        default: 'Bericht',
+    },
+    fullColls: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+let success = ref(false);
+let loaded = ref(true);
+let error = ref(false);
+
+let name = ref('');
+let company = ref('');
+let email = ref('');
+let phone = ref('');
+let message = ref('');
+
+function sendEmail(e) {
+    if (loaded) {
+        loaded.value = false;
+        success.value = false;
+        emailjs.sendForm('service_jgsniw7', 'template_bjcrtfm', e.target, 'user_UDbcG4RfnxXHwOYkfG6t6')
+            .then((result) => {
+                console.debug('SUCCESS!', result.status, result.text);
+                name.value = '';
+                email.value = '';
+                company.value = '';
+                phone.value = '';
+                message.value = '';
+                loaded.value = true;
+                success.value = true;
+                error.value = false;
+            }, (err) => {
+                loaded.value = true;
+                success.value = false;
+                error.value = true;
+                console.error('FAILED...', err);
+            });
+    }
+}
+</script>
